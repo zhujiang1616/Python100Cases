b12c0a0c53398890b3c3b36d6fe119a27f21d311
zhujiang1616 <45556206+zhujiang1616@users.noreply.github.com>
2019-07-09 08:01:25 +0800

    Update 006.py

--- 100examples/006.py
+++ 100examples/006.py
@@ -1,3 +1,4 @@
+'''
 def Fib(n):
     return 1 if n<=2 else Fib(n-1)+Fib(n-2)
 print(Fib(int(input())))
@@ -9,3 +10,26 @@ a,b=1,1
 for i in range(target-1):
     a,b=b,a+b
 print(a)
+'''
+'''
+# def fib(max):
+#     i,a,b = 0,1,1
+#     while i < max:
+#         print(b)
+#         a,b = b, a+b
+#         i +=1
+#     return 'done'
+# f = fib(12)
+# print(f)
+'''
+'''
+# def fib(max):
+#     i,a,b = 0,1,1
+#     while i < max:
+#         yield b
+#         a,b = b, a+b
+#         i +=1
+#     return 'done'
+# for n in fib(8):
+#     print(n)
+'''
