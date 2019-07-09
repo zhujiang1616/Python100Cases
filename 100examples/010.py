c449a2ecb3cd2a305bc7955794df969bf9609688
zhujiang1616 <45556206+zhujiang1616@users.noreply.github.com>
2019-07-09 08:23:12 +0800

    Update 010.py

--- 100examples/010.py
+++ 100examples/010.py
@@ -1,5 +1,14 @@
+'''
 import time
 
 for i in range(4):
     print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
     time.sleep(1)
+'''
+
+'''
+# import time
+# a = time.asctime(time.localtime())
+# time.sleep(1)
+# print('给你看的时间：', a)
+'''
